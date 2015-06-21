081458f2090864bac18906d23ba16c9c373fbcca
lamiaa-elmorsy <lamia.elmorsy@gmail.com>
2015-06-21 03:21:30 +0200

    update Selection

--- src/relop/Selection.java
+++ src/relop/Selection.java
@@ -6,57 +6,79 @@ package relop;
  * connected by AND operators.
  */
 public class Selection extends Iterator {
+	// TODO
+	private Iterator iter;
+	private Predicate[] preds;
 
-  /**
-   * Constructs a selection, given the underlying iterator and predicates.
-   */
-  public Selection(Iterator iter, Predicate... preds) {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Constructs a selection, given the underlying iterator and predicates.
+	 */
+	public Selection(Iterator iter, Predicate... preds) {
+		this.iter = iter;
+		this.preds = preds;
+		// Type mismatch: cannot convert from Predicate[] to Predicate
 
-  /**
-   * Gives a one-line explaination of the iterator, repeats the call on any
-   * child iterators, and increases the indent depth along the way.
-   */
-  public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+		// TODO: check again
+		this.setSchema(this.iter.getSchema());
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Restarts the iterator, i.e. as if it were just constructed.
-   */
-  public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Gives a one-line explaination of the iterator, repeats the call on any
+	 * child iterators, and increases the indent depth along the way.
+	 */
+	public void explain(int depth) {
+		throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Returns true if the iterator is open; false otherwise.
-   */
-  public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Restarts the iterator, i.e. as if it were just constructed.
+	 */
+	public void restart() {
+		if (isOpen()) {
+			close();
+		}
+		throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Closes the iterator, releasing any resources (i.e. pinned pages).
-   */
-  public void close() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Returns true if the iterator is open; false otherwise.
+	 */
+	public boolean isOpen() {
+		// TODO: Done
+		if (iter.isOpen()) {
+			return true;
+		} else {
+			return false;
+		}
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Returns true if there are more tuples, false otherwise.
-   */
-  public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Closes the iterator, releasing any resources (i.e. pinned pages).
+	 */
+	public void close() {
+		// TODO: Done
+		iter.close();
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Gets the next tuple in the iteration.
-   * 
-   * @throws IllegalStateException if no more tuples
-   */
-  public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Returns true if there are more tuples, false otherwise.
+	 */
+	public boolean hasNext() {
+		// TODO:
+		throw new UnsupportedOperationException("Not implemented");
+	}
+
+	/**
+	 * Gets the next tuple in the iteration.
+	 * 
+	 * @throws IllegalStateException
+	 *             if no more tuples
+	 */
+	public Tuple getNext() {
+		throw new UnsupportedOperationException("Not implemented");
+	}
 
 } // public class Selection extends Iterator
